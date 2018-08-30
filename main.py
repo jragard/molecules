185f7382df2b69b5581345bb724d403a7244c8ba
Ryan <jragard@gmail.com>
2018-08-30 17:12:14 -0400

    ugh

--- main.py
+++ main.py
@@ -37,6 +37,9 @@ def rectangles():
     
 def possible_molecules(first_vertical, first_horizontal, second_vertical, second_horizontal, poss_area):
     
+    area_width = poss_area[0]
+    area_height = poss_area[1]
+    
     areas = []
     
     one_two = compare_chains(first_vertical, first_horizontal)
@@ -45,62 +48,100 @@ def possible_molecules(first_vertical, first_horizontal, second_vertical, second
     three_four = compare_chains(second_vertical, second_horizontal)
     match = False
     
-    print 'one_two', one_two
-    print 'two_three', two_three
-    print '-----------'
-    print 'one_four', one_four
-    print '------------'
-    print 'three_four', three_four
+    # print 'one_two', one_two
+    # print '-------------'
+    # print 'two_three', two_three
+    # print '-----------'
+    # print 'one_four', one_four
+    # print '------------'
+    # print 'three_four', three_four
                                   
     for x in one_two:
         if 0 in x or 11 in x:
             continue
         else:
+            x_horiz_width = x[1]
+            x_vert_height = x[2]
             for tuple in two_three:
                 if 0 in tuple or 11 in tuple:
                     continue
                 else:
-                    if tuple[1] == poss_area[0] + x[1] + 1:
+                    tuple_horiz_width = tuple[1]
+                    tuple_vert_height = tuple[2]
+                    if tuple_horiz_width == area_width + x_horiz_width + 1:
                         for tuple2 in three_four:
                             if 0 in tuple2 or 11 in tuple2:
                                 continue
                             else:
-                                if tuple2[1] == poss_area[1] + tuple[2] + 1:
+                                tuple2_horiz_width = tuple2[2]
+                                tuple2_vert_height = tuple2[1]
+                                if tuple2_vert_height == area_height + x_vert_height + 1:
+                                      
                                       for tuple3 in one_four:
                                             if 0 in tuple3 or 11 in tuple3:
                                                 continue
                                             else:
-                                                if tuple3[2] == poss_area[1] + x[2] + 1:
+                                                tuple3_horiz_width = tuple3[1]
+                                                tuple3_vert_height = tuple3[2]
+                                      
+                                                if (tuple3_vert_height == area_height + x_vert_height + 1) and (tuple2_horiz_width - tuple3_horiz_width == area_width + 1):
                                                     match = True
-                                                    
-                                                    
+    
+    # area_width = 5
+    # area_height = 6
+    # x = ('D', 3, 3)
+    # tuple = ('J', 9, 3)
+    # tuple2 = ('B', 10, 9)
+    # tuple3 = ('N', 3, 10)
+    
+    # x_horiz_width = x[1]
+    # x_vert_height = x[2]
+    
+    # tuple_horiz_width = tuple[1]
+    # tuple_vert_height = tuple[2]
+    
+    # tuple2_horiz_width = tuple2[2]
+    # tuple2_vert_height = tuple2[1]
+    
+    # tuple3_horiz_width = tuple3[1]
+    # tuple3_vert_height = tuple3[2]
+    
+    # if tuple_horiz_width == area_width + x_horiz_width + 1:
+    #     print 'yay1'
+    #     if tuple2_vert_height == area_height + x_vert_height + 1:
+    #         print 'yay2'
+    #         if (tuple3_vert_height == area_height + x_vert_height + 1) and (tuple2_horiz_width - tuple3_horiz_width == area_width + 1):
+    #               return 'yes'
+    #         else:
+    #             print area_width
+                
                         
                                 
     if match:
-        print first_vertical, first_horizontal, second_vertical, second_horizontal, poss_area
-        print x
-        print tuple
-        print tuple2
-        print tuple3
         areas.append(poss_area[0]*poss_area[1])
+        print chain1, chain2, chain3, chain4, poss_area
         match = False
     
-    if len(areas) > 0:
+    if areas:
         return areas
     else:
         return 0
-                
+
+
+# print possible_molecules(chain1, chain2, chain3, chain4, (5, 6))                
     
-print possible_molecules('CHJDBJMHPJKD', 'OIMDIHEIAFNL', 'KAINLHLOLBEJ', 'LCBJOJGIEKBO',  (6, 6))
+# print possible_molecules('CHJDBJMHPJKD', 'OIMDIHEIAFNL', 'KAINLHLOLBEJ', 'LCBJOJGIEKBO',  (6, 6))
+
+# print possible_molecules('OIMDIHEIAFNL', 'CHJDBJMHPJKD', 'LCBJOJGIEKBO', 'KAINLHLOLBEJ', (5, 7))
 
 area_lst = []
 
-# for tuple in rectangles():
-#     i = itertools.permutations([chain1, chain2, chain3, chain4])
-#     for each in i:
-#         x = possible_molecules(each[0], each[1], each[2], each[3], tuple)
-#         if x != 0:
-#             area_lst.append(x)
-#             break
+for tuple in rectangles():
+    i = itertools.permutations([chain1, chain2, chain3, chain4])
+    for each in i:
+        x = possible_molecules(each[0], each[1], each[2], each[3], tuple)
+        if x != 0:
+            area_lst.append(x)
+            break
             
-# print area_lst
+print area_lst
