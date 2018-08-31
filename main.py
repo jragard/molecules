2bbe4591cdb5f24f8c97f070bc328dcf45fab360
Ryan <jragard@gmail.com>
2018-08-31 17:12:21 -0400

    all test cases work except the last one 'BBBABBBABBBB', 'CCACCCACCCCC', 'DDDDADDADDDD', 'EEAEEAEEEEEE'

--- main.py
+++ main.py
@@ -1,21 +1,46 @@
 import itertools
-
+import math
 
 chain1 = 'OIMDIHEIAFNL'
 chain2 = 'CHJDBJMHPJKD'
 chain3 = 'LCBJOJGIEKBO'
 chain4 = 'KAINLHLOLBEJ'
-
-
-def compare_chains(string1, string2):
-    lst = []
-    for index, char in enumerate(string1):
-        if char in string2:
-            lst.append((char, index, string2.index(char)))
     
-    return lst
-
-
+def crossings(horizontal, vertical, (width, height)):
+  
+    horizontal_lst = []
+    vertical_lst = []
+    final_lst = []
+    
+    max_horizontal_index = (len(horizontal) - 3) - width
+    max_vertical_index = (len(vertical) - 3) - height
+    
+    for index, char in enumerate(horizontal):
+        if index > max_horizontal_index:
+            break
+        else:
+            if char in vertical:
+                if index != 0 and index <= max_horizontal_index:
+                    if vertical.index(char) != 0 and vertical.index(char) <= max_vertical_index:
+                        horizontal_lst.append((char, index, vertical.index(char)))
+                        
+    for index, char in enumerate(vertical):
+        if index > max_vertical_index:
+            break
+        else:
+            if char in horizontal:
+                if index != 0 and index <= max_vertical_index:
+                    if horizontal.index(char) != 0 and horizontal.index(char) <= max_horizontal_index:
+                        vertical_lst.append((char, horizontal.index(char), index))
+                        
+    for x in horizontal_lst:
+        final_lst.append(x)
+        
+    for x in vertical_lst:
+        final_lst.append(x)
+                   
+    return list(set(final_lst))
+    
 
 def rectangles():
     lst = [(i, y) for i in range(1, 9) for y in range(i, 9)]  
@@ -29,84 +54,61 @@ def possible_molecules(first_vertical, first_horizontal, second_vertical, second
     
     areas = []
     
-    one_two = compare_chains(first_vertical, first_horizontal)
-    two_three = compare_chains(first_horizontal, second_vertical)
-    one_four = compare_chains(first_vertical, second_horizontal)
-    three_four = compare_chains(second_vertical, second_horizontal)
+    possible_crossings = crossings(first_horizontal, first_vertical, (area_width, area_height))
     match = False
-                                  
-    for x in one_two:
-        if 0 in x or 11 in x:
-            continue
-        else:
-            x_horiz_width = x[2]
-            x_vert_height = x[1]
-            for tuple1 in two_three:
-                if 0 in tuple1 or 11 in tuple1:
-                    continue
-                else:
-                    tuple1_horiz_width = tuple1[1]
-                    tuple1_vert_height = tuple1[2]
-                    if tuple1_horiz_width == area_width + x_horiz_width + 1:
-                        for tuple2 in three_four:
-                            if 0 in tuple2 or 11 in tuple2:
-                                continue
-                            else:
-                                tuple2_horiz_width = tuple2[2]
-                                tuple2_vert_height = tuple2[1]
-                                if tuple2_vert_height == area_height + x_vert_height + 1:
-                                      
-                                      for tuple3 in one_four:
-                                            if 0 in tuple3 or 11 in tuple3:
-                                                continue
-                                            else:
-                                                tuple3_horiz_width = tuple3[2]
-                                                tuple3_vert_height = tuple3[1]
-                                      
-                                                if tuple3_vert_height == tuple2_vert_height and tuple2_horiz_width - tuple3_horiz_width == area_width + 1:
-                                                    match = True
-                                                    
-
-                    
+    
+    try:
+        for crossing in possible_crossings:
+            horizontal = crossing[1]
+            vertical = crossing[2]
+            second_horiz_crossing = first_horizontal[horizontal + area_width + 1]
+            second_vert_crossing = second_vertical[vertical]
+            if first_horizontal[horizontal + area_width + 1] == second_vertical[vertical]:
+                bottom_vert_crossing = second_vertical[vertical + area_height + 1]
+                bottom_second_horiz = second_horizontal[horizontal + area_width + 1]
+                # if second_vertical[vertical + area_height + 1] == second_horizontal[horizontal + area_width + 1]:
+                if second_vertical[vertical + area_height + 1] in second_horizontal and first_vertical[vertical + area_height + 1] in second_horizontal:
+                    # x = second_horizontal[index + area_width + 1]
+                    # y = second_vertical[vertical + area_height + 1]
+                    final_vert_crossing = first_vertical[vertical + area_height + 1]
+                    for index, char in enumerate(second_horizontal):
+                        if char == first_vertical[vertical + area_height + 1] and second_horizontal[index + area_width + 1] == second_vertical[vertical + area_height + 1]:
+                    # if second_horizontal[horizontal] == first_vertical[vertical + area_height + 1]:
+                            match = True
+                            break
+    except:
+        return 0
+    
+    
     if match:
-        areas.append(poss_area[0]*poss_area[1])
-        print chain1, chain2, chain3, chain4, poss_area
-        match = False
+        areas.append(area_width*area_height)
+        print first_vertical, first_horizontal, second_vertical, second_horizontal, poss_area
+        
     
     if areas:
+        # print areas
         return areas
     else:
         return 0
 
-# print possible_molecules(chain1, chain2, chain3, chain4, (5, 6))                
-    
-# print possible_molecules('CHJDBJMHPJKD', 'OIMDIHEIAFNL', 'KAINLHLOLBEJ', 'LCBJOJGIEKBO',  (6, 6))
 
-# print possible_molecules('OIMDIHEIAFNL', 'CHJDBJMHPJKD', 'LCBJOJGIEKBO', 'KAINLHLOLBEJ', (5, 7))
-
-# print possible_molecules('OIMDIHEIAFNL', 'CHJDBJMHPJKD', 'LCBJOJGIEKBO', 'KAINLHLOLBEJ', (2, 5))
-
-# print possible_molecules('ABAAAAAAAABA', 'CBCCCCCCCCBC', 'DBDDDDDDDDBD', 'EBEEEEEEEEBE', (8,8))
+# print possible_molecules('ABAAAAAAAABA', 'CBCCCCCCCCBC', 'DBDDDDDDDDBD', 'EBEEEEEEEEBE', (8, 8))                
 
 area_lst = []
 
-for dimensions in rectangles():
-    i = itertools.permutations([chain1, chain2, chain3, chain4])
+for tup in rectangles():
+    # i = itertools.permutations(['CDBADCBBEFEF', 'DACCBADAFEAB', 'EFBDCAADBDCD', 'ABCDABCDABCD'])
+    # i = itertools.permutations(['DACCBADAFEAB', 'EFBDCAADBDCD', 'ABCDABCDABCD', 'CDBADCBBEFEF'])
+    # i = itertools.permutations(['ABABABABABAB', 'CDCDCDCDCDCD', 'EEEEEEEEEEEE', 'FFFFFFFFFFFF'])
+    # i = itertools.permutations(['ABAAAAAAAABA', 'CBCCCCCCCCBC', 'DBDDDDDDDDBD', 'EBEEEEEEEEBE'])
+    # i = itertools.permutations(['ABBBBBBBBBBA', 'ACCCCCCCCCCA', 'ADDDDDDDDDDA', 'AEEEEEEEEEEA'])
+    # i = itertools.permutations(['BBBABBBABBBB', 'CCACCCACCCCC', 'DDDDADDADDDD', 'EEAEEAEEEEEE'])
+    # i = itertools.permutations([chain1, chain2, chain3, chain4])
     for a,b,c,d in i:
-        x = possible_molecules(a,b,c,d, dimensions)
+        x = possible_molecules(a, b, c, d, tup)
         # print x
-        if x != 0:
+        if x:
             area_lst.append(x)
             break
-if area_lst:        
-    print max(area_lst)
-else:
-    print 0
-
+            
 print area_lst
-
-
-# CDBADCBBEFEF
-# DACCBADAFEAB
-# EFBDCAADBDCD
-# ABCDABCDABCD
